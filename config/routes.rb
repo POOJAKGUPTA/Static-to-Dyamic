d2839e157f1a511ce14f4a13606f7e367fb549d6
Poojak gupta <poojakgupta4@gmail.com>
2019-04-17 19:41:18 +0530

    logout button

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Myapp::Application.routes.draw do
  
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  devise_for :users
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+end
   # You can have the root of your site routed with "root"
-  root to: 'dashboards#dashboard_1'
+  root to: 'commerce#products_list'
 
   # All routes
   get "dashboards/dashboard_1"
